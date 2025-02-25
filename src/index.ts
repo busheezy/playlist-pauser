b7779369a12f29f54a73caa945407c050fbcebba
BuSheeZy <6915170+busheezy@users.noreply.github.com>
2025-02-25 13:27:26 -0600

    log events

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,22 @@ import { pause, play } from '@telemetrytv/sdk';
 const timeoutTime = 600;
 let timeout: number | null = null;
 
+function log(message: string) {
+  console.log(`[Pause Timeout]: ${message}`);
+}
+
 function handleTouchEvent() {
   if (timeout) {
     clearTimeout(timeout);
+    log('Reset timeout.');
   } else {
     pause();
+    log('Paused due to touch event.');
   }
 
   timeout = setTimeout(() => {
     play();
+    log('Resumed after timeout.');
     timeout = null;
   }, timeoutTime);
 }
