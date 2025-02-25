612612b46713a742d58db28b2ada12750ce9d659
BuSheeZy <6915170+busheezy@users.noreply.github.com>
2025-02-25 12:08:08 -0600

    start of basic logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hi");
+import { pause, play } from '@telemetrytv/sdk';
+
+const timeoutTime = 600;
+let timeout: number | null = null;
+
+function handleTouchEvent() {
+  if (timeout) {
+    clearTimeout(timeout);
+  } else {
+    pause();
+  }
+
+  timeout = setTimeout(() => {
+    play();
+    timeout = null;
+  }, timeoutTime);
+}
+
+document.addEventListener('touchstart', handleTouchEvent);
+document.addEventListener('touchmove', handleTouchEvent);
+document.addEventListener('touchend', handleTouchEvent);
+document.addEventListener('touchcancel', handleTouchEvent);
